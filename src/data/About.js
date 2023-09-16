b47d48b98e6d8fb4120544ccb9e99a0b70688145
Akash Gupta <skygupta0@gmail.com>
2023-09-17 01:50:41 +0530

    Feat: dynamic data and layout resposivness

---
+++ src/data/About.js
@@ -0,0 +1,23 @@
+export const about = {
+	firstName: "Akash",
+	lastName: "Gupta",
+	gender: "male",
+	// primaryProfileImage:
+	// 	"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674477439/Portfolio/profile-Image.jpg",
+	// secondaryProfileImage:
+	// 	"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674477439/Portfolio/profile-Image.jpg",
+	email: "akash.work@gmail.com",
+	shortDesc:
+		"An India-based freelancer who's into web development and open to work as a front-end developer.",
+	longDesc: [
+		"I'm an undergrad, self taught web developer from Pune, India. I possess skilled talent and immense experience in creating and maintaining visually appealing and user-friendly websites. I have comprehensive knowledge of various web development technologies such as HTML, CSS, JavaScript, ReactJS, Tailwind CSS, Firebase and more.",
+		"Overall, I guarantee the highest level of professionalism, technical proficiency, and client satisfaction in every aspect of web development.",
+	],
+	socialLinks: {
+		linkedin: "https://www.linkedin.com/in/skygupta0",
+		github: "https://github.com/skygupta0",
+		instagram: "https://www.instagram.com/akash.gupta",
+	},
+	resumeLink:
+		"https://firebasestorage.googleapis.com/v0/b/akashs-portfolio.appspot.com/o/Akash%20Rajesh%20Gupta.pdf?alt=media&token=361168f2-05eb-4039-91b6-b827c27c7457",
+};
