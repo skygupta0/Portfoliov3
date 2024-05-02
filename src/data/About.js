fc197d1de47a633e54f80138391ae81c90a7b627
Akash Gupta <skygupta0@gmail.com>
2024-05-02 17:19:54 +0530

    update About.js, projects.js and skills.js

--- src/data/About.js
+++ src/data/About.js
@@ -21,7 +21,7 @@ export const about = {
 		// twitter: "https://www.twitter.com/akash.gupta",
 	},
 	resumeLink:
-		"https://firebasestorage.googleapis.com/v0/b/akashs-portfolio.appspot.com/o/Akash%20Rajesh%20Gupta.pdf?alt=media&token=361168f2-05eb-4039-91b6-b827c27c7457",
+		"https://firebasestorage.googleapis.com/v0/b/akashs-portfolio.appspot.com/o/Akash%20Rajesh%20Gupta.pdf?alt=media&token=d07cc578-fb96-49b4-a296-b52d1f79caaa",
 
 	hostedUrl: "https://akashgupta.netlify.app/",
 };
